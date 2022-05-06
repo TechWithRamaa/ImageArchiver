b68abb9c8c5848dc346d5770cafccfc04d7b19ea
EngineeringWithRamaa <rkramadhevi@gmail.com>
2022-05-06 20:50:59 +0530

    Spring Boot App - Bare

---
+++ src/main/java/com/engineeringwithramaa/imagearchiver/ImageArchiverApplication.java
@@ -0,0 +1,13 @@
+package com.engineeringwithramaa.imagearchiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageArchiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageArchiverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/engineeringwithramaa/imagearchiver/ImageArchiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engineeringwithramaa.imagearchiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageArchiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

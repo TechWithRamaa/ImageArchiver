5870774626460df2f8d7b62d46e40a5b82d5261a
EngineeringWithRamaa <rkramadhevi@gmail.com>
2022-05-07 16:17:29 +0530

    POST api added for uploading user profile image

--- src/main/java/com/engineeringwithramaa/imagearchiver/profile/UserProfileService.java
+++ src/main/java/com/engineeringwithramaa/imagearchiver/profile/UserProfileService.java
@@ -2,8 +2,10 @@ package com.engineeringwithramaa.imagearchiver.profile;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class UserProfileService {
@@ -17,5 +19,14 @@ public class UserProfileService {
 
     List<UserProfile> getUserProfiles() {
         return userProfileDataAccessService.getUserProfiles();
+    }
+
+    public void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
+        //1 check if image is present
+        //2 if file is an image
+        //3 the user exists in our fake database
+        //4 grab metadata from file if any
+        //5 store image in s3 and update profile image link in fake db
+
     }
 }
